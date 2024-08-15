f9aa72b49e6708147c02d3c705f3d672831f35e7
Angel Morales <mbaangel@hotmail.com>
2024-08-14 21:24:00 -0600

    Primer commit

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+from config import config
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['inputEmail'])
+        print(request.form['inputPassword'])
+        return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.run()
